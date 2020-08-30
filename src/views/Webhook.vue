82c33547c5a7d40949632c6506e899de8b954271
Alpha-mAxXx <mb0420141@gmail.com>
2020-08-30 23:13:42 +0200

    newdss

---
+++ src/views/Webhook.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>In work fuccer</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+      
+  }
+}
+</script>
