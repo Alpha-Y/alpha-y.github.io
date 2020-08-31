256277360e0853d173df6022b3ce4391c1e24b08
Alpha <60668473+Alpha-Y@users.noreply.github.com>
2020-08-31 13:02:07 +0200

    Update Webhook.vue
    ewds

--- src/views/Webhook.vue
+++ src/views/Webhook.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Discord Webhook Creator</h1>
+    
+    <div>
+
+      <div>
+        <label>Webhook URL</label>
+        <input type="url">
+      </div>
+
+      <div>
+        <label>Message</label>
+        <input >
+      </div>      
+    </div>
     <h1>In work fuccer</h1>
   </div>
 </template>
